c380ac969d68b22548adf719f362702614619007
bblaha <bblaha@users.noreply.github.com>
2018-08-14 17:20:07 +0200

    Added stop function

---
+++ stop.php
@@ -0,0 +1,91 @@
+<?php
+function callAPI($method, $url, $data){
+   $curl = curl_init();
+   
+   switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "PUT":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);                         
+         break;
+      default:
+         if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+   }
+   // OPTIONS:
+   curl_setopt($curl, CURLOPT_URL, $url);
+   /*curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'APIKEY: 111111111111111111111',
+      'Content-Type: application/json',
+   ));*/
+   curl_setopt($curl, CURLOPT_HTTPHEADER, $data);
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+   // EXECUTE:
+   $result = curl_exec($curl);
+   if(!$result){die("Connection Failure");}
+   curl_close($curl);
+   return $result;
+}
+
+function getPayload(){
+		
+	$mail = $_GET["mail"];
+	$key = $_GET["key"];
+	$payload = array(
+      'X-ClockodoApiUser: '.$mail,
+      'X-ClockodoApiKey: '.$key,
+   );
+   
+   return $payload;
+}
+
+
+function main(){
+
+
+	$jsonTxt = json_decode(callAPI("GET","https://my.clockodo.com/api/clock", getPayload()), true);
+	var_dump(json_decode(callAPI("GET","https://my.clockodo.com/api/clock/".$jsonTxt["running"]["id"], getPayload()), true));
+	
+	
+	
+}
+
+?>
+<html>
+<head>
+<META HTTP-EQUIV="refresh" CONTENT="10">
+<style>
+body{
+	background-color:black;
+	color:white;
+	font-size:1.5em;
+	font-family: Arial;
+	}
+.service{
+	font-size:0.36em;
+}
+.namebox{
+	display:block;
+	float:left;
+	background-color:red;
+	width:200px;
+	height:75px;
+	margin:5px;
+	text-align:center;
+	vertical-align:center;
+}
+.active{
+	background-color:green;
+}
+</style>
+</head>
+<body>
+	<?php main() ?>
+</body>
+</html>
