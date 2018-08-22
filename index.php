6c6b32307c7d267cf3266ed615ad0fc4331c78af
bblaha <bblaha@users.noreply.github.com>
2018-08-22 18:06:09 +0200

    removed debugging output

--- index.php
+++ index.php
@@ -87,8 +87,7 @@ function main(){
 		?>
 			<div class="namebox<?php if($working){echo " active";}; ?>">
 		<?php echo $user["name"]; ?><br />
-		<?php echo $admin;?>
-		<?php if($working&&$admin=="true"){echo "<p class=\"service\">(".getServiceObject($clockObject).")</p>";}; ?>
+		<?php if($working&&$admin){echo "<p class=\"service\">(".getServiceObject($clockObject).")</p>";}; ?>
 			</div>
 		<?php
 		}
